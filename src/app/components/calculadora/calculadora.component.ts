390539488286dfae5320e71b0815a1a0e9a89a05
Roberto Rebolledo <robertorebolledo151@gmail.com>
2023-10-16 22:00:01 +0200

    no more time

--- src/app/components/calculadora/calculadora.component.ts
+++ src/app/components/calculadora/calculadora.component.ts
@@ -4,19 +4,46 @@ import { Component } from '@angular/core';
   selector: 'app-calculadora',
   templateUrl: './calculadora.component.html',
   styleUrls: ['./calculadora.component.css']
-})
-export class CalculadoraComponent {
+})export class CalculatorComponent {
+  public result: number = 0;
+  public currentNumber: string = '';
+  public operator: string = '';
+  public firstNumber: number =0;
 
-  private resultado: number;
-  private num1: number;
-  private num2: number;
-
-  constructor() {
-    this.num1 = 0;
-    this.num2 = 0;
-    this.resultado = 0;
+  public appendNumber(number: string): void {
+    this.currentNumber = this.currentNumber.concat(number);
   }
 
+  public clear(): void {
+    this.currentNumber = '';
+    this.operator = '';
+    this.firstNumber = 0;
+  }
 
+  public setOperator(operator: string): void {
+    this.operator = operator;
+    this.firstNumber = parseFloat(this.currentNumber);
+    this.currentNumber = '';
+  }
 
+  public calculate(): void {
+    const secondNumber = parseFloat(this.currentNumber);
+    switch (this.operator) {
+      case '+':
+        this.result = this.firstNumber + secondNumber;
+        break;
+      case '-':
+        this.result = this.firstNumber - secondNumber;
+        break;
+      case '*':
+        this.result = this.firstNumber * secondNumber;
+        break;
+      case '/':
+        this.result = this.firstNumber / secondNumber;
+        break;
+    }
+    this.currentNumber = this.result.toString();
+    this.operator = '';
+    this.firstNumber = 0;
+  }
 }
